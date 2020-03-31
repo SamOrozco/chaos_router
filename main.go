349d14a90820cd2ee89a613e00819eb5f13e29e2
Sam Orozco <samuel.j.orozco@gmail.com>
2020-03-30 22:07:47 -0600

    finished with the first config

--- main.go
+++ main.go
@@ -1,52 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	config2 "go_chaos/config"
-	"go_chaos/routing_rules"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	config := config2.ChaosRouterConfig{
-		Port:             9001,
-		MaxPossibilities: 100,
-		RoutingRulesConfigs: []config2.RoutingRuleConfig{
-			{
-				Match: config2.MatchConfig{
-					Type:       "path",
-					MatchValue: "/athena/test",
-					MatchType:  "equals",
-				},
-				Route: routing_rules.Route{
-					Host: "192.168.0.34",
-					Port: 9001,
-				},
-			},
-			{
-				Match: config2.MatchConfig{
-					Type:       "path",
-					MatchValue: "/athena",
-					MatchType:  "equals",
-				},
-				Route: routing_rules.Route{
-					Host: "192.168.0.34",
-					Port: 9002,
-				},
-			},
-		},
-		ChaosRuleConfigs: []config2.ChaosRuleConfig{
-			{
-				Percent:            50,
-				ResponseStatusCode: 200,
-				ResponseBody:       "success",
-			},
-			{
-				Percent:            50,
-				ResponseStatusCode: 400,
-				ResponseBody:       "bad request",
-			},
-		},
+	fileLocation := getFileLocationFromArgs()
+	// read bytes from file
+	data, err := ioutil.ReadFile(fileLocation)
+	if err != nil {
+		panic(err)
 	}
 	
-	chaosRouter := config2.CreateChaosRouterFromConfig(config)
+	config := &config2.ChaosRouterConfig{}
+	if err := json.Unmarshal(data, config); err != nil {
+		panic(err)
+	}
+	
+	chaosRouter := config2.CreateChaosRouterFromConfig(*config)
 	chaosRouter.Start()
+}
+
+func getFileLocationFromArgs() string {
+	args := os.Args
+	if len(args) < 2 {
+		panic("config file location not given as program argument")
+	}
+	return args[1]
 }
